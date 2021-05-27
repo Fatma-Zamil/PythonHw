87a94fb15c3d80f6b064b5528632951a9f0d9be9
Fatma-Zamil <fatmazamil55@gmail.com>
2021-05-27 22:45:34 +0300

    Create Registration form Task2.py

---
+++ Registration form Task2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 21:49:02 2021
+
+@author: AMCT
+"""
+
+
+#this is a registration form that collects some data 
+
+print("\nRegistration form")
+Name=input('Insert your full name here\n')
+Date=input('Insert your birth date here\n')
+E_mail=input('Insert your E-mail here\n')
+print("\nThank you")
+
+
